282eaf247354960a69cbfcfbc69038ee9d3badc5
Anton <reopik@mail.ru>
2023-11-26 17:02:55 +0300

    added server actions: delete and create board

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,33 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { Board } from "./board";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
-      Organization Page
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board 
+            key={board.id} 
+            id={board.id} 
+            title={board.title} />
+        ))}
+      </div>
     </div>
   )
 }
